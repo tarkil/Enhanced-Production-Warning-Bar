b1f94d44c997f8d311cf4c002b31023c24e99f0c
tarkil <phantomquest@gmail.com>
2017-11-20 21:07:07 +0100

    issue #28 Minor refactor and complete warning modal tests

--- test/components/warningModal.spec.js
+++ test/components/warningModal.spec.js
@@ -8,15 +8,17 @@ import { shallow } from 'enzyme';
 import JestReactRendererUtils from '../utils/jestReactRendererUtils'
 
 describe("WarningModal", () => {
-    it('Modal is rendered correctly when it is created and continue buttom is clicked', () => {
+    it('Modal is rendered correctly when it is created and continue button is clicked', () => {
         //When: modal is created
         const component = renderer.create(
             <WarningModal />
         );
         //Then:
         expect(component.toJSON()).toMatchSnapshot();
+
         //And: Continue button is clicked
-        const button = JestReactRendererUtils.selectComponent(component, 'Dimmer > Container > Button')[1]; //component.toTree().rendered.props.children[1].props.children[1].props.onClick();
+        //component.toTree().rendered.props.children[1].props.children[1].props.onClick();l
+        const button = JestReactRendererUtils.selectComponent(component, 'Dimmer > Container > Button')[1];
 
         button.props.onClick();
         //Then: modal disappear
@@ -24,22 +26,77 @@ describe("WarningModal", () => {
 
     });
 
+    it('Modal is rendered correctly when it is created and exit button is clicked', () => {
+        //When: modal is created
+        const component = renderer.create(
+            <WarningModal />
+        );
+        //Then:
+        expect(component.toJSON()).toMatchSnapshot();
+
+        //And: Exit button is clicked
+        const button = JestReactRendererUtils.selectComponent(component, 'Dimmer > Container > Button')[0];
+
+        button.props.onClick();
+        //Then: modal disappear
+        expect(component.toJSON()).toMatchSnapshot();
+    });
+
+
     it('Modal should block web page and disappear when we click on continue', () => {
         //Given:
-        const component = shallow(<WarningModal />);
+        const onShowFunctionMock = jest.fn();
+        const onContinueFunctionMock = jest.fn();
+        const onexitFunctionMock = jest.fn();
+        const component = shallow(<WarningModal onShow={onShowFunctionMock} onContinue={onContinueFunctionMock} onExit={onexitFunctionMock} />);
 
         //then: Dimmer should block user interaction with the current web page
         let dimmer = component.find('Dimmer');
         expect(dimmer.props().active).toBeTruthy();
+        expect(onShowFunctionMock.mock.calls.length).toBe(1);
+        expect(onContinueFunctionMock.mock.calls.length).toBe(0);
+        expect(onexitFunctionMock.mock.calls.length).toBe(0);
+
 
         //and: if the continue button is clicked
         const continueButton = component.find('Button').last();
         continueButton.simulate('click');
         component.update();
 
-        //Then: modal dissapears
+        //Then: modal disappears
         dimmer = component.find('Dimmer');
         expect(dimmer.props().active).toBeFalsy();
+        expect(onShowFunctionMock.mock.calls.length).toBe(1);
+        expect(onContinueFunctionMock.mock.calls.length).toBe(1);
+        expect(onexitFunctionMock.mock.calls.length).toBe(0);
+    });
 
+
+    it('Modal should block web page and exit when we click on continue', () => {
+        //Given:
+        const onShowFunctionMock = jest.fn();
+        const onContinueFunctionMock = jest.fn();
+        const onexitFunctionMock = jest.fn();
+        const component = shallow(<WarningModal onShow={onShowFunctionMock} onContinue={onContinueFunctionMock} onExit={onexitFunctionMock} />);
+
+        //then: Dimmer should block user interaction with the current web page
+        let dimmer = component.find('Dimmer');
+        expect(dimmer.props().active).toBeTruthy();
+        expect(onShowFunctionMock.mock.calls.length).toBe(1);
+        expect(onContinueFunctionMock.mock.calls.length).toBe(0);
+        expect(onexitFunctionMock.mock.calls.length).toBe(0);
+
+
+        //and: if the exit button is clicked
+        const continueButton = component.find('Button').first();
+        continueButton.simulate('click');
+        component.update();
+
+        //Then: modal disappears
+        dimmer = component.find('Dimmer');
+        expect(dimmer.props().active).toBeTruthy();
+        expect(onShowFunctionMock.mock.calls.length).toBe(1);
+        expect(onContinueFunctionMock.mock.calls.length).toBe(0);
+        expect(onexitFunctionMock.mock.calls.length).toBe(1);
     });
 });

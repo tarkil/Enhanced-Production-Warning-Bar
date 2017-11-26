3e47060e20ae20eb61c3f1d471e11203c045bdfa
tarkil <phantomquest@gmail.com>
2017-11-26 13:34:17 +0100

    issue #28 remove old scripts and include translations

--- script/build.sh
+++
@@ -1,4 +0,0 @@
-#!/usr/bin/env bash
-mkdir -p build/js
-./node_modules/.bin/browserify -o build/js/content.js src/js/content.js -t babelify
-./node_modules/.bin/browserify   src/js/application.jsx  -o build/js/application-bundle.js -t babelify
